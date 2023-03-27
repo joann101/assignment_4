55dc8d3b0355f59e8029b9337020d8d53d2f3f0e
joann101 <joanndinzey@gmail.com>
2023-03-26 23:31:51 -0400

    Sort functions, parse function, and extramemallocated complete

---
+++ compareSortAlgorithms.c
@@ -0,0 +1,232 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
+
+int extraMemoryAllocated;
+
+// implement merge sort
+// extraMemoryAllocated counts bytes of extra memory allocated
+void mergeSort(int pData[], int l, int r)
+{
+	if (l < r) {
+        int m = l + (r - l) / 2;
+  
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+
+        int i, j, k;
+        int num1 = m - l + 1;
+        int num2 = r - m;
+ 
+        int L[num1], R[num2];
+        extraMemoryAllocated = (sizeof(L)+sizeof(R))*sizeof(int);
+
+        for (i = 0; i < num1; i++){
+            L[i] = pData[l + i];
+        }
+        for (j = 0; j < num2; j++){
+            R[j] = pData[m + 1 + j];
+        }
+
+        i = 0; 
+        j = 0; 
+        k = l; 
+        while (i < num1 && j < num2) {
+            if (L[i] <= R[j]) {
+                pData[k] = L[i];
+                i++;
+            }
+            else {
+                pData[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+  
+        while (i < num1) {
+            pData[k] = L[i];
+            i++;
+            k++;
+        }
+  
+        while (j < num2) {
+            pData[k] = R[j];
+            j++;
+            k++;
+        }  
+    }
+    extraMemoryAllocated = extraMemoryAllocated-(sizeof(int)*(r+1));
+}
+
+// implement insertion sort
+// extraMemoryAllocated counts bytes of memory allocated
+void insertionSort(int* pData, int n)
+{
+    extraMemoryAllocated=n*sizeof(int);
+	int x, k;
+    for (int i = 1; i < n; i++) {
+        x = pData[i];
+        k = i - 1;
+
+        while (k >= 0 && pData[k] > x) {
+            pData[k+1] = pData[k];
+            k=k-1;
+        }
+        pData[k+1] = x;
+    }
+    extraMemoryAllocated = n*sizeof(int)-extraMemoryAllocated;
+}
+
+// implement bubble sort
+// extraMemoryAllocated counts bytes of extra memory allocated
+void bubbleSort(int* pData, int n)
+{
+    extraMemoryAllocated=n*sizeof(int);
+    int i, k;
+    for (i = 0; i < n - 1; i++){
+        for (k = 0; k < n - i - 1; k++){
+            if (pData[k] > pData[k + 1]){
+                int tmp = pData[k];
+                pData[k] = pData[k + 1];
+                pData[k + 1] = tmp;
+            }
+        }
+    extraMemoryAllocated = n*sizeof(int)-extraMemoryAllocated;
+    }
+}
+
+// implement selection sort
+// extraMemoryAllocated counts bytes of extra memory allocated
+void selectionSort(int* pData, int n)
+{
+    extraMemoryAllocated=n*sizeof(int);
+    int i, j, min;
+    for (i = 0; i < n-1; i++){
+        min = i;
+
+        for (j = i+1; j < n; j++){
+
+            if (pData[j] < pData[min]){
+            min = j;
+            }
+        }
+      
+        if(min != i){
+            int tmp = pData[min];
+            pData[min] = pData[i];
+            pData[i] = tmp;
+        }
+    extraMemoryAllocated = n*sizeof(int)-extraMemoryAllocated;
+    }
+}
+
+// parses input file to an integer array
+int parseData(char *inputFileName, int **ppData)
+{
+	FILE* inFile = fopen(inputFileName,"r");
+	int dataSz = 0;
+	*ppData = NULL;
+	
+	if (inFile)
+	{
+	    fscanf(inFile,"%d\n",&dataSz);
+		*ppData = (int *)malloc(sizeof(int) * dataSz);
+		
+        // Implement parse data block
+        for (int i = 0; i<dataSz; i++){
+            fscanf(inFile,"%d", &((*ppData)[i]));
+        }
+	}
+    
+	return dataSz;
+}
+
+// prints first and last 100 items in the data array
+void printArray(int pData[], int dataSz)
+{
+	int i, sz = dataSz - 100;
+	printf("\tData:\n\t");
+	for (i=0;i<100;++i)
+	{
+		printf("%d ",pData[i]);
+	}
+	printf("\n\t");
+	
+	for (i=sz;i<dataSz;++i)
+	{
+		printf("%d ",pData[i]);
+	}
+	printf("\n\n");
+}
+
+int main(void)
+{
+	clock_t start, end;
+	int i;
+    double cpu_time_used;
+	char* fileNames[] = {"input1.txt", "input2.txt", "input3.txt"};
+	
+	for (i=0;i<3;++i)
+	{
+		int *pDataSrc, *pDataCopy;
+		int dataSz = parseData(fileNames[i], &pDataSrc);
+		
+		if (dataSz <= 0)
+			continue;
+		
+		pDataCopy = (int *)malloc(sizeof(int)*dataSz);
+	
+		printf("---------------------------\n");
+		printf("Dataset Size : %d\n",dataSz);
+		printf("---------------------------\n");
+		
+		printf("Selection Sort:\n");
+		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
+		extraMemoryAllocated = 0;
+		start = clock();
+		selectionSort(pDataCopy, dataSz);
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
+		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
+		printArray(pDataCopy, dataSz);
+		
+		printf("Insertion Sort:\n");
+		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
+		extraMemoryAllocated = 0;
+		start = clock();
+		insertionSort(pDataCopy, dataSz);
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
+		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
+		printArray(pDataCopy, dataSz);
+
+		printf("Bubble Sort:\n");
+		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
+		extraMemoryAllocated = 0;
+		start = clock();
+		bubbleSort(pDataCopy, dataSz);
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
+		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
+		printArray(pDataCopy, dataSz);
+		
+		printf("Merge Sort:\n");
+		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
+		extraMemoryAllocated = 0;
+		start = clock();
+		mergeSort(pDataCopy, 0, dataSz - 1);
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
+		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
+		printArray(pDataCopy, dataSz);
+		
+		free(pDataCopy);
+		free(pDataSrc);
+	}
+	
+}
